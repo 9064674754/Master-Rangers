de64e7b14769280f8423ad8779881783ccee04b7
9064674754 <98456102+9064674754@users.noreply.github.com>
2022-03-16 14:57:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+var canvas = new fabric.Canvas("myCanvas");
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
 var block_image_object= "";
 
 function new_image(get_image)
